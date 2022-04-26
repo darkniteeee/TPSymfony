88f90d5d4edb404622b6d6366bd14ec40c23c043
tvoltz2021 <thomas.voltz@outlook.fr>
2022-04-26 12:00:34 +0200

    modif controller

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -6,12 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
+
+/**
+ * @Route(name="accueil_")
+ */
+class AccueilController extends AbstractController
 {
     /**
-     * @Route(name="home", path:'')
+     * @Route(name="", path:'')
      */
-    public function home()
+    public function Accueil()
     {
         return $this->render('main/home.html.twig');
     }
