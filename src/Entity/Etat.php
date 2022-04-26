f6388157c45068da4cb7d2a2b947d9d2a6da7532
tdedours2021 <titdedours@gmail.com>
2022-04-26 15:52:01 +0200

    Etat "à jour"

--- src/Entity/Etat.php
+++ src/Entity/Etat.php
@@ -4,38 +4,47 @@ namespace App\Entity;
 
 use App\Repository\EtatRepository;
 use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Integer;
 
 /**
  * @ORM\Entity(repositoryClass=EtatRepository::class)
+ * @ORM\Table(name="Etats")
  */
 class Etat
 {
     /**
      * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(name="id", type="integer", options={"unsigned": true})
      */
-    private $id;
+    private ?Integer  $id=null;
 
     /**
-     * @ORM\Column(type="string", length=30)
+     * @ORM\Column(name="libelle", type="string", length=30, unique=true)
      */
-    private $libelle;
+    private ?Integer $libelle=null;
 
+    /**
+     * @return int|null
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @return string|null
+     */
     public function getLibelle(): ?string
     {
         return $this->libelle;
     }
 
-    public function setLibelle(string $libelle): self
+    /**
+     * @param string $libelle
+     */
+    public function setLibelle(string $libelle): void
     {
         $this->libelle = $libelle;
-
-        return $this;
     }
 }
